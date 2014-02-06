cc4c5a709fc4e73214a0abe5e6f9a862309d8bef
yambi <yambian@gmail.com>
2014-02-07 02:19:00 +0900

    add trash.sh

---
+++ trash.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+TRASH_DIR=~/.Trash/
+
+while [[ $1 = -* ]]
+do
+    shift
+done
+
+if [ $# -eq 0 ]
+then
+    echo "usage: $0 file ..."
+    exit 1
+fi
+
+for file in $@
+do
+    if [ ! -e $file ]
+    then
+        echo "$0: $file: No such file or directory"
+        exit 1
+    fi
+done
+dst_dir=$TRASH_DIR$(date +%Y%m%d-%H%M%S)
+if [ -e dst_dir ]; then :
+else
+    mkdir "$dst_dir"
+fi
+echo "trash.sh: $@ -> $dst_dir"
+mv $@ $dst_dir
